2de8ef19f489149f057b2b972a0f83f78fa3b681
Edwin Clement <edwinclement08@gmail.com>
2020-10-31 18:00:43 +0530

    Removed Better-modules-aliases

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-require('better-module-alias')(__dirname);
-
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes');
 const usersRouter = require('./routes/users');
 
 const app = express();
